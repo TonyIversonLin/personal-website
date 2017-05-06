a019c16b1004e4520a0fccf157651b6034c577fd
Tony Lin <tonyiversonlin@gmail.com>
2017-05-05 22:52:35 -0400

    add smooth animation on about page

--- utility.js
+++ utility.js
@@ -3,15 +3,32 @@ let height = window.innerHeight
 							|| document.documentElement.clientHeight
 							|| document.body.clientHeight;
 let activationStatus = {
-	nav: true
+	nav: true,
+	about: true,
+	skill: true
 }
+
 let navElement = document.querySelector('nav');
+let aboutElement = document.querySelector('#about');
+let skillElement = document.querySelector('#skill')
+let aboutActivationLocation = aboutElement.offsetTop - (3/5) * height;
+let skillActivationLocation = skillElement.offsetTop - (3/5) * height;
 
 
 function activateAnimation() {
-	console.log('start scrolling')
+	console.log('start scrolling');
+	let currentLocation = body.scrollTop;
 	if(activationStatus.nav){
 		navElement.className += ' on-scroll';
 		activationStatus.nav = false;
 	}
+	if(activationStatus.about && currentLocation >= aboutActivationLocation) {
+		aboutElement.className += ' active';
+		activationStatus.about = false;
+	}
+	if(activationStatus.skill && currentLocation >= skillActivationLocation) {
+		skillElement.className += ' active';
+		activationStatus.skill = false;
+	}
 }
+
